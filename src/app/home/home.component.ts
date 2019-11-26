3c41216e0ed2f83c1f7ed8570068282bffed665c
cedjustin <cedro265@gmail.com>
2019-11-26 17:40:09 +0200

    started a new UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade',
+      [
+        state('void', style({ opacity: 0 })),
+        transition(':enter', [animate(300)]),
+        transition(':leave', [animate(500)]),
+      ]
+    )]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document) { }
 
   title = 'Fedha';
   nameBrand = 'FEDHA';
@@ -17,6 +27,17 @@ export class HomeComponent implements OnInit {
   emailAddress = 'example@example.com';
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    if (window.pageYOffset > 600) {
+      const element = document.getElementById('navbar');
+      element.classList.add('sticky');
+    } else {
+      const element = document.getElementById('navbar');
+      element.classList.remove('sticky');
+    }
   }
 
 }
